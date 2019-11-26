f144d1f3c0168b19f48d36de53a5c0b57141dc55
uakci <uakci@uakci.pl>
2019-11-26 23:51:40 +0100

    fixes #9 (‘undesirable word-internal breaks’)

--- assets/accents.js
+++ assets/accents.js
@@ -1,14 +1,35 @@
-(function onload() {
-  var distinctAccents = document.querySelectorAll('strong');
-  for(var i = 0; i < distinctAccents.length; i++) {
-    distinctAccents[i].innerHTML = distinctAccents[i].innerHTML
-      // .replace(/[auıoey]/g, '<span class="t t8" data-content="$&">$&</span>')
-      .replace(/[āūīōēȳ]/g, '&#8288;<span class="t t1" data-content="$&">$&</span>&#8288;')
-      .replace(/[áúíóéý]/g, '&#8288;<span class="t t2" data-content="$&">$&</span>&#8288;')
-      .replace(/[äüïöëÿ]/g, '&#8288;<span class="t t3" data-content="$&">$&</span>&#8288;')
-      .replace(/[ảủỉỏẻỷ]/g, '&#8288;<span class="t t4" data-content="$&">$&</span>&#8288;')
-      .replace(/[âûîôêŷ]/g, '&#8288;<span class="t t5" data-content="$&">$&</span>&#8288;')
-      .replace(/[àùìòèỳ]/g, '&#8288;<span class="t t6" data-content="$&">$&</span>&#8288;')
-      .replace(/[ãũĩõẽỹ]/g, '&#8288;<span class="t t7" data-content="$&">$&</span>&#8288;');
+function accentify(element) {
+  console.log(element, element.nodeType);
+  if(element.nodeType == Node.TEXT_NODE) {
+    var words = element.data.split(/ /);
+    var replacement = document.createElement('span');
+    for(var i = 0; i < words.length; i++) {
+      var toa = document.createElement('span');
+      toa.className = 'toa';
+      toa.innerHTML = words[i]
+     // .replace(/[auıoey]/g, '<span class="t t8" data-content="$&">$&</span>')
+        .replace(/[āūīōēȳ]/g, '<span class="t t1" data-content="$&">$&</span>')
+        .replace(/[áúíóéý]/g, '<span class="t t2" data-content="$&">$&</span>')
+        .replace(/[äüïöëÿ]/g, '<span class="t t3" data-content="$&">$&</span>')
+        .replace(/[ảủỉỏẻỷ]/g, '<span class="t t4" data-content="$&">$&</span>')
+        .replace(/[âûîôêŷ]/g, '<span class="t t5" data-content="$&">$&</span>')
+        .replace(/[àùìòèỳ]/g, '<span class="t t6" data-content="$&">$&</span>')
+        .replace(/[ãũĩõẽỹ]/g, '<span class="t t7" data-content="$&">$&</span>');
+      if(i > 0) {
+        replacement.appendChild(document.createTextNode(' '));
+      }
+      replacement.appendChild(toa);
+    }
+    console.log(replacement);
+    element.parentNode.replaceChild(replacement, element);
+  } else {
+    for(var i = 0; i < element.childNodes.length; i++) {
+      accentify(element.childNodes[i]);
+    }
   }
-})();
+}
+
+var distinctAccents = document.querySelectorAll('strong');
+for(var i = 0; i < distinctAccents.length; i++) {
+  accentify(distinctAccents[i]);
+}
